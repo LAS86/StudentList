eaa757a02238c81ddfae086518ab49049e34c53e
Luke <70299112+LAS86@users.noreply.github.com>
2022-09-21 19:26:36 -0500

    Frontend Files Uploaded

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import AppBar from './componenets/Appbar';
+import Student from './componenets/Student';
+
+function App() {
+  return (
+    <div className="App">
+      <AppBar/>
+      <Student/>
+    </div>
+  );
+}
+
+export default App;
